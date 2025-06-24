a2ac28cf12477a557a12c0582815abe5df4b2729
Vedhesh <vbyte.02@gmail.com>
2025-06-24 16:20:29 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let App = ()=>{
+  return(
+    <>
+      <h1 className='motion-preset-pop'>hello world</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import './index.css';
+
+// App
+import App from './App';
+
+let root = document.getElementById('root');
+
+createRoot(root).render(
+  <App/>
+);
