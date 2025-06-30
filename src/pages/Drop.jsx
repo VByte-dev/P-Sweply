b868b85a08979b80177e578590b9f87065128e72
Vedhesh <vbyte.02@gmail.com>
2025-06-30 16:22:55 +0530

    Drop

--- src/pages/Drop.jsx
+++ src/pages/Drop.jsx
@@ -46,26 +46,26 @@ let Drop = () => {
 
   return (
     <>
-      <div className="border-2 rounded-xl bg-zinc-50 border-zinc-200 px-6 py-6 text-md">
+      <div className="rounded-xl bg-zinc-50 border-2 border-zinc-200  px-6 py-6 text-md">
         {/* Thought tnput */}
         <div>
           <textarea
             name=""
             id="thoughtInput"
             placeholder="What’s on your mind?"
-            className="bg-white text-[#1F1F1F] h-32 w-full rounded-t-xl -xl py-3 px-3 sm:px-6 font-space outline-none border-2 border-zinc-200"
+            className="bg-white text-[#1F1F1F] h-32 w-full rounded-xl  py-3 px-3 sm:px-6 font-space outline-none border-2 border-zinc-200 resize-none"
             onChange={(e) => {
               handleThought(e.target.value);
             }}
           ></textarea>
         </div>
         {/* Tagging thought */}
-        <div className="w-full border-2 bg-white border-zinc-200   mt-2 py-3 px-3 sm:mt-4 sm:py-6 sm:px-6 flex justify-between overflow-auto text-sm">
+        <div className="w-full mt-2 flex-wrap text-sm">
           {TAGS.map((v, i, a) => (
             <h1
               key={i}
-              className={` text-[#1F1F1F] hover:bg-[#3A86FF] hover:text-[#F5F7FA] inline-block rounded py-1 px-4 m-2 font-space border-0 cursor-pointer  ${
-                tagInput.includes(v) ? "bg-[#3A86FF] text-white" : "bg-zinc-200"
+              className={` text-[#1F1F1F] hover:bg-[#3A86FF] hover:text-[#F5F7FA] inline-block rounded py-1 px-4 mr-2 sm:mr-4 my-2 font-space border-0 cursor-pointer  ${
+                tagInput.includes(v) ? "bg-black text-white" : "bg-zinc-200"
               }`}
               onClick={(e) => handleTag(v)}
             >
@@ -76,7 +76,7 @@ let Drop = () => {
         {/* Drop button */}
         <div>
           <button
-            className="w-full bg-[#3A86FF] font-space text-white rounded py-2 mt-6 active:bg-[#3a51ff] "
+            className="w-full bg-[#3A86FF] font-space text-white rounded py-1 mt-6 active:bg-[#3a51ff] "
             onClick={handleDrop}
           >
             Drop
